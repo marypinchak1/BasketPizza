e7866b90dff9dc0314eff1278c11191e4dc80afd
melkoff <vasamelko@gmail.com>
2023-03-15 13:55:41 +0200

    burger-adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,24 @@
-let slider = $(document).ready(function () {
-  $('.your-class').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: true,
-    infinite: true
-  });
-});
+// burger menu
+'use strict';
+const humb = document.querySelector('.burger');
+const humbBody = document.querySelector('.burger__menu');
+const header = document.querySelector('.header');
+const menuOpen = document.querySelector('html');
+const humbLink = document.querySelector('.burger__menu');
 
+humb.addEventListener('click', humbHandler);
+
+function humbHandler(burgerOn) {
+  burgerOn.preventDefault();
+  humbBody.classList.toggle("open");
+  header.classList.toggle('active');
+  menuOpen.classList.toggle("menu__open");
+  humb.classList.toggle('active');
+  humbLink.classList.toggle('active');
+}
+$('.burger__menu a').on('click', function () {
+  $('.burger__menu').toggleClass('open');
+  $('.burger').toggleClass('active');
+  $('html').toggleClass('menu__active');
+})
+// ============================================= //
